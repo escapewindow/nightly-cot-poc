1be900c6cc17f5ebe24a4010ed8fe12d9b9c4efe
Aki Sasaki <aki@escapewindow.com>
2016-05-31 23:20:08 -0700

    can't figure out how to read cleartext signed

--- verify.py
+++ verify.py
@@ -16,9 +16,13 @@ import pgpy
 #        contents += line.rstrip()
 
 #print(contents)
-kr = pgpy.PGPKeyring(glob.glob('gpg/*.gpg'))
+kr = pgpy.PGPKeyring(glob.glob('gpg/*ring.gpg'))
 message = pgpy.PGPMessage.from_file('decision.gpg')
 print(message.is_signed)
 print(message.signatures)
 with kr.key('docker1') as key:
     print(key.verify(message))
+
+#message = pgpy.PGPMessage.from_file('decision_cleartext.gpg')
+#with kr.key('docker1') as key:
+#    print(key.verify(message))
