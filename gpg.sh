c11dcafa70c1826ebf74b0e11d5694d12e460e49
Aki Sasaki <aki@escapewindow.com>
2016-08-17 00:08:01 -0700

    ownertrust working!

--- gpg.sh
+++ gpg.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-/usr/local/bin/gpg2 --no-default-keyring --secret-keyring gpg/secring.gpg --keyring gpg/pubring.gpg "$@"
+if [ "$HOMEDIR." == "." ] ; then
+    HOMEDIR=./gpg
+fi
+/usr/local/bin/gpg2 --homedir $HOMEDIR --no-default-keyring --secret-keyring $HOMEDIR/secring.gpg --keyring $HOMEDIR/pubring.gpg "$@"
--- keys.py
+++ keys.py
@@ -4,15 +4,17 @@
 import gnupg
 import logging
 import os
+# import pexpect
 import pprint
+import subprocess
 import tempfile
 
 log = logging.getLogger(__name__)
 
 # Constants {{{1
+GPG = '/usr/local/bin/gpg'
 MY_EMAIL = "scriptworker@example.com"
 KEYS_TO_TRUST = ('decision.root@example.com', 'build.root@example.com', 'docker.root@example.com')
-KEY_DIR = "/tmp"
 KEY_DATA = (
     # Root keys.  These sign the embedded keys.  We trust the root keys, and
     # the signature makes the embedded keys valid.
@@ -47,6 +49,7 @@ def write_key(gpg, keyid, path):
         print(ascii_armored_private_key, file=fh)
 
 
+# generate_keys {{{1
 def generate_keys(gpg):
     """ Generate the gpg keys from KEY_DATA
     """
@@ -60,21 +63,62 @@ def generate_keys(gpg):
     return fingerprints
 
 
+# update_trust {{{1
+def update_trust(gpg_path, gpg_home, emails):
+    log.info("Updating ownertrust...")
+    ownertrust = []
+    trustdb = os.path.join(gpg_home, "trustdb.gpg")
+    if os.path.exists(trustdb):
+        os.remove(trustdb)
+    # trust MY_EMAIL ultimately
+    ownertrust.append("{}:6\n".format(emails[MY_EMAIL]))
+    for email in KEYS_TO_TRUST:
+        ownertrust.append("{}:5\n".format(emails[email]))
+    ownertrust = ''.join(ownertrust).encode('utf-8')
+    print(ownertrust)
+    cmd = [
+        gpg_path,
+        "--homedir", gpg_home,
+        "--no-default-keyring",
+        "--secret-keyring", os.path.join(gpg_home, "secring.gpg"),
+        "--keyring", os.path.join(gpg_home, "pubring.gpg"),
+        "--import-ownertrust"
+    ]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout = p.communicate(input=ownertrust)[0]
+    if stdout:
+        log.info("gpg output:\n{}".format(stdout))
+    assert not p.returncode
+    print(subprocess.check_output(
+    [
+        gpg_path,
+        "--homedir", gpg_home,
+        "--no-default-keyring",
+        "--secret-keyring", os.path.join(gpg_home, "secring.gpg"),
+        "--keyring", os.path.join(gpg_home, "pubring.gpg"),
+        "--export-ownertrust"
+    ]))
+
+
 # main {{{1
 def main(name=None):
     log.setLevel(logging.INFO)
     log.addHandler(logging.StreamHandler())
-    with tempfile.TemporaryDirectory() as tmpdir:
+    tmpdir = tempfile.mkdtemp()
+    #with tempfile.TemporaryDirectory() as tmpdir:
+    try:
         gpg = gnupg.GPG(gnupghome=tmpdir)
         gpg.encoding = 'utf-8'
         fingerprints = generate_keys(gpg)
         emails = {v: k for k, v in fingerprints.items()}
+        update_trust(GPG, tmpdir, emails)
         for key in gpg.list_keys():
-            path = os.path.join(KEY_DIR, fingerprints[key['fingerprint']])
+            email = fingerprints[key['fingerprint']]
+            path = os.path.join(tmpdir, email)
             write_key(gpg, key['keyid'], path)
-        # TODO trust MY_EMAIL
-        # TODO sign KEYS_TO_TRUST
         # TODO tests!
+    finally:
+        log.info("Files are in {}".format(tmpdir))
 
 
 main()
