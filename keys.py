dfd548f4502dd68ff5b79639505e08fb483d8a74
Aki Sasaki <aki@escapewindow.com>
2016-08-16 23:10:47 -0700

    refactor keys.py; logging; start working on trust

--- keys.py
+++ keys.py
@@ -2,10 +2,17 @@
 """ Quick and dirty test gpg key generation script, for scriptworker testing
 """
 import gnupg
+import logging
 import os
 import pprint
 import tempfile
 
+log = logging.getLogger(__name__)
+
+# Constants {{{1
+MY_EMAIL = "scriptworker@example.com"
+KEYS_TO_TRUST = ('decision.root@example.com', 'build.root@example.com', 'docker.root@example.com')
+KEY_DIR = "/tmp"
 KEY_DATA = (
     # Root keys.  These sign the embedded keys.  We trust the root keys, and
     # the signature makes the embedded keys valid.
@@ -29,6 +36,9 @@ KEY_DATA = (
 
 # write_key {{{1
 def write_key(gpg, keyid, path):
+    """ Write the pub and sec keys for keyid to path.pub and path.sec
+    """
+    log.info("Writing keys to %s.{pub,sec}" % path)
     ascii_armored_public_key = gpg.export_keys(keyid)
     with open("{}.pub".format(path), "w") as fh:
         print(ascii_armored_public_key, file=fh)
@@ -37,23 +47,34 @@ def write_key(gpg, keyid, path):
         print(ascii_armored_private_key, file=fh)
 
 
+def generate_keys(gpg):
+    """ Generate the gpg keys from KEY_DATA
+    """
+    log.info("Generating keys...")
+    fingerprints = {}
+    for key_tuple in KEY_DATA:
+        k = dict(zip(("name_real", "name_comment", "name_email"), key_tuple))
+        k['key_length'] = 2048
+        key = gpg.gen_key(gpg.gen_key_input(**k))
+        fingerprints[key.fingerprint] = k['name_email']
+    return fingerprints
+
+
 # main {{{1
 def main(name=None):
-    fingerprints = {}
+    log.setLevel(logging.INFO)
+    log.addHandler(logging.StreamHandler())
     with tempfile.TemporaryDirectory() as tmpdir:
         gpg = gnupg.GPG(gnupghome=tmpdir)
         gpg.encoding = 'utf-8'
-        for key_tuple in KEY_DATA:
-            k = dict(zip(("name_real", "name_comment", "name_email"), key_tuple))
-            k['key_length'] = 2048
-            key = gpg.gen_key(gpg.gen_key_input(**k))
-            fingerprints[key.fingerprint] = k['name_email']
+        fingerprints = generate_keys(gpg)
         emails = {v: k for k, v in fingerprints.items()}
-        pprint.pprint(fingerprints)
-        pprint.pprint(emails)
         for key in gpg.list_keys():
-            path = os.path.join("/tmp", fingerprints[key['fingerprint']])
+            path = os.path.join(KEY_DIR, fingerprints[key['fingerprint']])
             write_key(gpg, key['keyid'], path)
+        # TODO trust MY_EMAIL
+        # TODO sign KEYS_TO_TRUST
+        # TODO tests!
 
 
 main()
